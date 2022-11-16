aaff03f458998655593a4ba0f1ac19591d5d1771
adel <adel.albediwy@gmail.com>
2022-11-16 19:54:46 +0300

    Attempt

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <bits/stdc++.h>
+#include <future>
+
+void test(){
+    auto threadId = std::this_thread::get_id();
+    std::cout<<"Test on thread: "<<threadId<<std::endl;
+    std::this_thread::sleep_for(std::chrono::seconds(5));
+}
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    auto threadId = std::this_thread::get_id();
+    std::cout<<"Main thread: "<<threadId<<std::endl;
+
+    for(int i=0;i<10;i++){
+        auto future = std::async(std::launch::async, test);
+    }
     return 0;
 }
