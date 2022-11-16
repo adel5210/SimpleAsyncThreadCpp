c9868e182d61d629afc222a001a82adbadf1c174
adel <adel.albediwy@gmail.com>
2022-11-16 07:26:43 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/SimpleAsyncThread.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SimpleAsyncThread.dir/main.cpp.o"
+  "SimpleAsyncThread"
+  "SimpleAsyncThread.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SimpleAsyncThread.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
